1a3fdb26fa4b88f9ca55cc3e634639caa88cc14a
Mati Machado <matiasmachado7@gmail.com>
2021-07-16 20:01:19 -0300

    chore: init commit (#1)

--- src/Control_ALU.v
+++ src/Control_ALU.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.07.2021 15:51:41
-// Design Name: 
-// Module Name: Control_ALU
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Control_ALU
     #(
