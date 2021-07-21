32ccc68ca10c50d7c649a06d1a597eb941a412f5
Lucas Blanco <lucasblanco3107@gmail.com>
2021-07-21 20:49:36 -0300

    chore: Se implementa logica de decoder. (#4)
    Co-authored-by: Lucas Blanco <lucasblanco31@gmail.com>

--- src/Control_Unidad.v
+++ src/Control_Unidad.v
@@ -19,24 +19,25 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+//LW:   100011  | base  |   RT  |   OFFSET
+//SW:   101011  |  base |   RT  |   OFFSET
+//ADD:  000000  |   RS  |   RT  |   RD  |   00000   |   100000
+//SUB:  000000  |   RS  |   RT  |   RD  |   00000   |   100010
+//AND:  000000  |   RS  |   RT  |   RD  |   00000   |   100100
+//OR:   000000  |   RS  |   RT  |   RD  |   00000   |   100101
+//SLT:  000000  |   RS  |   RT  |   RD  |   00000   |   101010
+//BEQ:  000100  |   RS  |   RT  |   OFFSET   
+
+`define LW  6'b100011
+`define SW  6'b101011
+`define BEQ 6'b000100
 
 module Control_Unidad
     #(
-        parameter   NBITS       =   32  ,
-        parameter   NBITS_I     =   32  ,
-        parameter   OPCODE      =   6   ,
-        parameter   RS          =   5   ,
-        parameter   RT          =   5   ,
-        parameter   RD          =   5   ,
-        parameter   DIRECCION   =   16  ,
-        parameter   SHAMT       =   5   ,
-        parameter   FUNCT       =   6   
+        parameter   NBITS       =   6 
     )
     (
-        input   wire                            i_clk           ,
-        input   wire                            i_reset         ,
-        input   wire    [NBITS_I-1      :0]     i_Instruction   ,
-        input   wire                            i_NPC           ,
+        input   wire    [NBITS-1        :0]     i_Instruction   ,
         
         output  wire                            o_RegDst        ,
         output  wire                            o_Branch        ,
@@ -45,33 +46,78 @@ module Control_Unidad
         output  wire    [1              :0]     o_ALUOp         ,
         output  wire                            o_MemWrite      ,
         output  wire                            o_ALUSrc        ,
-        output  wire                            o_RegWrite      ,
-        output  wire    [OPCODE-1       :0]     o_Opcode        ,
-        output  wire    [RS-1           :0]     o_Rs            ,
-        output  wire    [RT-1           :0]     o_Rt            ,
-        output  wire    [RD-1           :0]     o_Rd            ,
-        output  wire    [DIRECCION-1    :0]     o_Direccion     
+        output  wire                            o_RegWrite      
     );
     
-    reg         [NBITS-1  :0]         pc_reg;
+    reg         RegDst_Reg      ;
+    reg         Branch_Reg      ;
+    reg         MemRead_Reg     ;
+    reg         MemToReg_Reg    ;
+    reg [1:0]   ALUOp_Reg       ;
+    reg         MemWrite_Reg    ;
+    reg         ALUSrc_Reg      ;
+    reg         RegWrite_Reg    ;
     
-    assign  o_Opcode    =   i_Instruction[NBITS-1               :RS+RT+DIRECCION    ];
-    assign  o_Rs        =   i_Instruction[NBITS-OPCODE-1        :RT+DIRECCION       ];
-    assign  o_Rt        =   i_Instruction[NBITS-OPCODE-RS-1     :DIRECCION          ];
-    assign  o_Rd        =   i_Instruction[NBITS-OPCODE-RS-RT-1  :SHAMT+FUNCT        ];
-    assign  o_Direccion =   i_Instruction[NBITS-OPCODE-RS-RT-1  :0                  ];
-       
-    //always @(negedge i_clk) definir en que flanco aumenta el pc
-    always @(*)
-    begin
-        if(i_reset)
-        begin
-            pc_reg          <=      {NBITS{1'b0}}   ;
-        end
-        //else if(wr_pc)
-        else 
-            pc_reg          <=      i_NPC           ;
-    end
+    assign  o_RegDst    =   RegDst_Reg      ;
+    assign  o_Branch    =   Branch_Reg      ;
+    assign  o_MemRead   =   MemRead_Reg     ;
+    assign  o_MemToReg  =   MemToReg_Reg    ;
+    assign  o_ALUOp     =   ALUOp_Reg       ;
+    assign  o_MemWrite  =   MemWrite_Reg    ;
+    assign  o_ALUSrc    =   ALUSrc_Reg      ;
+    assign  o_RegWrite  =   RegWrite_Reg    ;
 
-    
+    always @(*)
+    begin : Decoder
+        case(i_Instruction)
+        000000:       
+            RegDst_Reg      <=  1'b1    ;
+            Branch_Reg      <=  1'b0    ;
+            MemRead_Reg     <=  1'b0    ; 
+            MemToReg_Reg    <=  1'b0    ;
+            ALUOp_Reg       <=  2'b10   ;
+            MemWrite_Reg    <=  1'b0    ;
+            ALUSrc_Reg      <=  1'b0    ;
+            RegWrite_Reg    <=  1'b1    ;
+        
+        `LW:       
+            RegDst_Reg      <=  1'b0    ;
+            Branch_Reg      <=  1'b0    ;
+            MemRead_Reg     <=  1'b1    ; 
+            MemToReg_Reg    <=  1'b1    ;
+            ALUOp_Reg       <=  2'b00   ;
+            MemWrite_Reg    <=  1'b0    ;
+            ALUSrc_Reg      <=  1'b1    ;
+            RegWrite_Reg    <=  1'b1    ;
+
+        `SW:       
+            RegDst_Reg      <=  1'b0    ;
+            Branch_Reg      <=  1'b0    ;
+            MemRead_Reg     <=  1'b0    ; 
+            MemToReg_Reg    <=  1'b0    ;
+            ALUOp_Reg       <=  2'b00   ;
+            MemWrite_Reg    <=  1'b1    ;
+            ALUSrc_Reg      <=  1'b1    ;
+            RegWrite_Reg    <=  1'b0    ;
+
+        `BEQ:       
+            RegDst_Reg      <=  1'b0    ;
+            Branch_Reg      <=  1'b1    ;
+            MemRead_Reg     <=  1'b0    ; 
+            MemToReg_Reg    <=  1'b0    ;
+            ALUOp_Reg       <=  2'b01   ;
+            MemWrite_Reg    <=  1'b0    ;
+            ALUSrc_Reg      <=  1'b1    ;
+            RegWrite_Reg    <=  1'b0    ;
+
+        default:       
+            RegDst_Reg      <=  1'b0    ;
+            Branch_Reg      <=  1'b0    ;
+            MemRead_Reg     <=  1'b0    ; 
+            MemToReg_Reg    <=  1'b0    ;
+            ALUOp_Reg       <=  2'b00   ;
+            MemWrite_Reg    <=  1'b0    ;
+            ALUSrc_Reg      <=  1'b0    ;
+            RegWrite_Reg    <=  1'b0    ;         
+    end
 endmodule
