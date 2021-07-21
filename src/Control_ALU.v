4124f9bdb3829f72486a17f166c4b7c189336be8
Mati Machado <matias@craftech.io>
2021-07-21 20:46:14 -0300

    chore : Agrego casteo de variables

--- src/Control_ALU.v
+++ src/Control_ALU.v
@@ -1,5 +1,14 @@
 `timescale 1ns / 1ps
 
+//Definicion de Macros mediante directiva 'define'
+//<nombre>	<tamao>' <base>
+`define	ADD	    6'b100000	//Suma
+`define	SUB	    6'b100010	//Resta
+`define	AND	    6'b100100	//And
+`define	OR	    6'b100101	//Or
+`define	SLT	    6'b101010	//Set on less than
+
+
 module Control_ALU
     #(
         parameter   NBITS           =   6   ,
@@ -24,14 +33,14 @@ module Control_ALU
                                     ALUOp_Reg   <=   4'b0110    ;
                 10:
                     case(i_Funct)
-                        100000:     ALUOp_Reg   <=   4'b0010    ;
-                        100010:     ALUOp_Reg   <=   4'b0110    ;
-                        100100:     ALUOp_Reg   <=   4'b0000    ;
-                        100101:     ALUOp_Reg   <=   4'b0001    ;
-                        101010:     ALUOp_Reg   <=   4'b0111    ;
-                        default:    ALUOp_Reg   <=   -1         ;
+                        `ADD :     ALUOp_Reg   <=   4'b0010    ;
+                        `SUB :     ALUOp_Reg   <=   4'b0110    ;
+                        `AND :     ALUOp_Reg   <=   4'b0000    ;
+                        `OR  :     ALUOp_Reg   <=   4'b0001    ;
+                        `SLT :     ALUOp_Reg   <=   4'b0111    ;
+                        default:    ALUOp_Reg   <=   -1        ;
                     endcase       
-                default:            ALUOp_Reg   <=   -1         ;
+                default:            ALUOp_Reg   <=   -1        ;
             endcase
     end
 endmodule
