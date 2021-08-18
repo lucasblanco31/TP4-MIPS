763228c21777b90ac0457792f0716e6a0618fdc5
Lucas Blanco <lucasblanco3107@gmail.com>
2021-08-18 14:33:48 -0300

    Chore: Se agrega señal de Jump a la unidad de control. Y se arregla error en Multiplexor Jump.

--- src/Mux_PC_Jump.v
+++ src/Mux_PC_Jump.v
@@ -8,7 +8,7 @@ module Mux_PC_Jump
     (
         input   wire                            i_Jump          ,
         input   wire    [NBITSJUMP-1    :0]     i_IJump         ,
-        input   wire    [NBITS-1        :0]     i_PC+4          ,
+        input   wire    [NBITS-1        :0]     i_PC_4          ,
         input   wire    [NBITS-1        :0]     i_SumadorBranch ,
         output  wire    [NBITS-1        :0]     o_PC                 
     );
@@ -20,7 +20,7 @@ module Mux_PC_Jump
     begin
         case(i_Jump)
             1'b0:   to_PC   <=  i_SumadorBranch                     ;   
-            1'b1:   to_PC   <=  {i_PC+4[NBITS-1:27], (i_IJump<<2)}  ;
+            1'b1:   to_PC   <=  {i_PC_4[NBITS-1:27], (i_IJump<<2)}  ;
         endcase
     end   
 endmodule
