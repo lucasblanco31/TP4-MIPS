4f6c56e973389b891b497663e94b051fb084357e
matiri132 <matias@craftech.io>
2023-04-10 10:21:25 -0300

    chore: refactor files mux

--- src/Mux_PC.v
+++ src/Mux_PC.v
@@ -1,23 +1,34 @@
 `timescale 1ns / 1ps
+
 module Mux_PC
     #(
-        parameter NBITS = 32
+        parameter NBITS     =  32
     )
     (
-        input   wire                        i_PCSrc         ,
-        input   wire    [NBITS-1      :0]   i_SumadorBranch ,
-        input   wire    [NBITS-1      :0]   i_SumadorPC4    ,
-        output  wire    [NBITS-1      :0]   o_MuxPC                 
+        input   wire                            i_Jump          ,
+        input   wire                            i_PCSrc         ,
+        input   wire    [NBITS-1      :0]       i_SumadorBranch ,
+        input   wire    [NBITS-1      :0]       i_SumadorPC4    ,        
+        input   wire    [NBITS-1      :0]       i_SumadorJump   ,
+        output  wire    [NBITS-1      :0]       o_PC            
     );
     
-    reg [NBITS-1  :0]       MuxPC_reg   ;    
-    assign  o_MuxPC   =     MuxPC_reg   ;
+    reg             [NBITS-1  :0]          PC_reg   ;
+   
+     
+    assign          o_PC             =     PC_reg   ;
+      
     
     always @(*)
     begin
-        if(i_PCSrc)
-            MuxPC_reg   <=  i_SumadorBranch ;
+        
+        if(i_Jump)
+            PC_reg      <=  i_SumadorJump   ;
+        else if(i_PCSrc)
+            PC_reg      <=  i_SumadorBranch ;
         else
-            MuxPC_reg   <=  i_SumadorPC4    ;
+            PC_reg      <=  i_SumadorPC4    ;
+             
+            
     end   
 endmodule
