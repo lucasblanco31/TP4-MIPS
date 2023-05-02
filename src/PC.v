0a36e4f088109fc5e72594354c76cdf47bc3c7a7
matiri132 <matias@craftech.io>
2023-05-02 00:54:50 -0300

    chore: add reset to PC

--- src/PC.v
+++ src/PC.v
@@ -6,8 +6,8 @@ module PC
         parameter   NBITS       =   32  
     )
     (
-        input   wire                            i_clk           ,
-        input   wire                            i_reset         ,
+        input   wire                            clk             ,
+        input   wire                            reset           ,
         input   wire    [NBITS-1    :0]         i_NPC           ,
         output  wire    [NBITS-1    :0]         o_PC            ,
         output  wire    [NBITS-1    :0]         o_PC_4          ,
@@ -19,14 +19,18 @@ module PC
     assign  o_PC     =   PC_Reg     ;       
     assign  o_PC_4   =   PC_Reg + 4 ;
     assign  o_PC_8   =   PC_Reg + 8 ;        
-       
-    always @(negedge i_clk)
+    
+    always @(posedge reset)
     begin
-        if(i_reset) begin
-            PC_Reg          <=      {NBITS{1'b0}}    ;
-        //else if(wr_pc)
-        end
-        else 
-            PC_Reg          <=      i_NPC           ;
+        
     end
+
+    always @(negedge clk, posedge reset)
+    begin
+        if(reset) begin
+            PC_Reg          <=      0         ;
+        end else begin
+            PC_Reg          <=      i_NPC     ;
+        end
+    end  
 endmodule
