0b117fc8efe9fda7e4957251b53e9c50def38597
Lucas Blanco <lucas@craftech.io>
2023-05-03 14:48:57 -0300

    main: fix Mux PC

--- src/Mux_PC.v
+++ src/Mux_PC.v
@@ -6,10 +6,12 @@ module Mux_PC
     )
     (
         input   wire                            i_Jump          ,
+        input   wire                            i_JALR          ,
         input   wire                            i_PCSrc         ,
         input   wire    [NBITS-1      :0]       i_SumadorBranch ,
         input   wire    [NBITS-1      :0]       i_SumadorPC4    ,        
         input   wire    [NBITS-1      :0]       i_SumadorJump   ,
+        input   wire    [NBITS-1      :0]       i_RS            ,
         output  wire    [NBITS-1      :0]       o_PC            
     );
     
@@ -24,6 +26,8 @@ module Mux_PC
         
         if(i_Jump)
             PC_reg      <=  i_SumadorJump   ;
+        else if (i_JALR)
+            PC_reg      <=  i_RS            ;
         else if(i_PCSrc)
             PC_reg      <=  i_SumadorBranch ;
         else
