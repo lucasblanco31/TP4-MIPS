b4eaea9f28998dba1fc69f342377ca06da22bf09
Lucas Blanco <lucas@craftech.io>
2023-05-08 17:48:54 -0300

    chore: add debug unit fix (#31)
    * chore(add-debug-unit-fix): resolved conflicts
    
    * chore(add-debug-unit-fix): resolved conflicts in TOP MIPS
    
    * chore: update memoria de datos reset
    
    * chore: update memoria de datos reset
    
    * chore: update memoria de registros reset
    
    ---------
    
    Co-authored-by: matiri132 <matias@craftech.io>

--- src/Memoria_Datos.v
+++ src/Memoria_Datos.v
@@ -1,58 +1,36 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.07.2021 15:11:16
-// Design Name: 
-// Module Name: Mux_Memoria
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module Memoria_Datos
     #(
         parameter NBITS     = 32    ,
-        parameter CELDAS    = 10
+        parameter CELDAS    = 16
     )
     (
         input   wire                        i_clk           ,
         input   wire                        i_reset         ,
         input   wire    [NBITS-1    :0]     i_ALUDireccion  ,
+        input   wire    [NBITS-1    :0]     i_DebugDireccion,
         input   wire    [NBITS-1    :0]     i_DatoRegistro  ,
         input   wire                        i_MemWrite      ,
         input   wire                        i_MemRead       ,
-        output  wire    [NBITS-1    :0]     o_DatoLeido         
+        output  wire    [NBITS-1    :0]     o_DatoLeido     ,
+        output  wire    [NBITS-1    :0]     o_DebugDato
     );
     
     reg     [NBITS-1  :0]     memory[CELDAS-1:0];
     reg     [NBITS-1  :0]     dato;
+    reg     [NBITS-1  :0]     debug_dato;
     integer i;
     
-    assign o_DatoLeido = dato;
+    assign o_DatoLeido  = dato;
+    assign o_DebugDato  = debug_dato;
     
     initial 
     begin
-        memory[0]       <=      32'b0000_0000_0000_0000_0000_0000_0000_0000 ; 
-        memory[1]       <=      32'b0000_0000_0000_0000_0000_0000_0000_0001 ; 
-        memory[2]       <=      32'b0000_0000_0000_0000_0000_0000_0000_0010 ; 
-        memory[3]       <=      32'b0000_0000_0000_0000_0000_0000_0000_0011 ; 
-        memory[4]       <=      32'b0000_0000_0000_0000_0000_0000_0000_0100 ;   
-        memory[5]       <=      32'b0000_0000_0000_0000_0000_0000_0000_0101 ; 
-        memory[6]       <=      32'b0000_0000_0000_0000_0000_0000_0000_0110 ; 
-        memory[7]       <=      32'b0000_0000_0000_0000_0000_0000_0000_0111 ; 
-        memory[8]       <=      32'b0000_0000_0000_0000_0000_0000_0000_1000 ; 
-        memory[9]       <=      32'b0000_0000_0000_0000_0000_0000_0000_1011 ;    
-        //instruction     <=      32'b0000_0000_0000_0000_0000_0000_0000_1011 ;      
+        for (i = 1; i <= CELDAS; i = i + 1) begin
+                memory[i] = i*2;
+        end
+        debug_dato  =  0;
     end
     
     always @(posedge i_reset)
@@ -64,10 +42,15 @@ module Memoria_Datos
                 memory[i] <= i;
             end
             dato         <=      {NBITS{1'b0}}    ;
+            debug_dato   <=      {NBITS{1'b0}}    ;
         end
     end
-    
-    //always @(posedge i_clk)
+
+    always @(i_DebugDireccion)
+    begin
+        debug_dato                  <=  memory[i_DebugDireccion];
+    end
+
     always @(*)
     begin
         if (i_MemRead)
@@ -75,6 +58,7 @@ module Memoria_Datos
             dato                    <=  memory[i_ALUDireccion];
         end 
     end
+
     always @(negedge i_clk)
     begin
         if(i_MemWrite)
--- src/Registros.v
+++ src/Registros.v
@@ -1,42 +1,23 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 16.07.2021 15:11:16
-// Design Name: 
-// Module Name: Mux_Memoria
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 module Registros
     #(
         parameter   REGS        =   5   ,
         parameter   NBITS       =   32  ,
-        parameter   RS          =   5   ,
-        parameter   RT          =   5   ,
-        parameter   RD          =   5   ,
         parameter   CELDAS      =   32  
     )
     (
-        input   wire                    i_clk           ,
-        input   wire                    i_reset         ,
-        input   wire                    i_RegWrite      , 
-        input   wire    [RS-1       :0] i_RS            , //Leer registro 1
-        input   wire    [RT-1       :0] i_RT            , //Leer registro 2
-        input   wire    [RD-1       :0] i_RD            , //Escribir registro
-        input   wire    [NBITS-1    :0] i_DatoEscritura , //Escribir dato
-        output  wire    [NBITS-1    :0] o_RS            , // Dato leido 1
-        output  wire    [NBITS-1    :0] o_RT              // Dato leido 2
+        input   wire                        i_clk           ,
+        input   wire                        i_reset         ,
+        input   wire                        i_RegWrite      ,
+        input   wire    [REGS-1         :0] i_RS            , //Leer registro 1
+        input   wire    [REGS-1         :0] i_RT            , //Leer registro 2
+        input   wire    [REGS-1         :0] i_RD            , //Escribir registro
+        input   wire    [REGS-1         :0] i_RegDebug      , //Leer registro debug
+        input   wire    [NBITS-1        :0] i_DatoEscritura , //Escribir dato
+        output  wire    [NBITS-1        :0] o_RS            , // Dato leido 1
+        output  wire    [NBITS-1        :0] o_RT            , // Dato leido 2
+        output  wire    [NBITS-1        :0] o_RegDebug
     );
     
     reg     [NBITS-1    :0]     memory[CELDAS-1:0]  ;
@@ -46,27 +27,14 @@ module Registros
     
     assign o_RS = Reg_RS;
     assign o_RT = Reg_RT;
+    assign o_RegDebug = Reg_Debug;
     
-//    //TODO: PONER EN CERO TODA LA MEMORIA
-//    initial 
-//    begin
-//        memory[0]       <=      32'b00;
-//        memory[1]       <=      32'b01; //32
-//        memory[2]       <=      32'b10; 
-//        memory[3]       <=      32'b11;
-//        memory[4]       <=      32'b100;
-//        memory[5]       <=      32'b101;
-//        memory[6]       <=      32'b110;
-//        memory[7]       <=      32'b111;
-//        memory[8]       <=      32'b1000;
-//        memory[9]       <=      32'b1001;
-//        memory[10]       <=      32'b1010;
-//        memory[11]       <=      32'b1011;
-//        memory[12]       <=      32'b1100;
-//        memory[13]       <=      32'b1101;
-//        memory[14]       <=      32'b1110;
-//        memory[15]       <=      32'b1111;
-//    end
+    initial
+    begin
+        for (i = 0; i < CELDAS; i = i + 1) begin
+                memory[i] = i;
+        end
+    end
     
     always @(posedge i_reset)
     begin
@@ -74,17 +42,21 @@ module Registros
         begin
             Reg_RS         <=   {NBITS{1'b0}}   ;
             Reg_RT         <=   {NBITS{1'b0}}   ;
+            o_RegDebug     <=   {NBITS{1'b0}}   ;
             for (i = 0; i < CELDAS-1; i = i + 1) 
             begin
                 memory[i] <= i;
             end
         end
     end
+
     always @(*)
     begin
-            Reg_RS      <=  memory[i_RS]    ;
-            Reg_RT      <=  memory[i_RT]    ;
+            Reg_RS      <=  memory[i_RS]        ;
+            Reg_RT      <=  memory[i_RT]        ;
+            Reg_Debug   <=  memory[i_RegDebug]  ;
     end
+
     always @(negedge i_clk)
     begin
         if(i_RegWrite)
